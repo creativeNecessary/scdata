9e3d9f62049dfa53540f91c04b4cfb6cca4bfe8e
EricAlexEX <>
2018-03-13 13:26:22 +0800

    new ships sq;

--- app/Http/Controllers/Api/ShipController.php
+++ app/Http/Controllers/Api/ShipController.php
@@ -17,14 +17,13 @@ class ShipController extends Controller
         $start = $page_num * $per_page;
         $ships = DB::select('select * from ship_en LIMIT ? , ?',[$start,$start+$per_page]);
         $ships = json_decode(json_encode($ships, true),true);
-        $test = array();
         foreach ($ships as $ship){
             $pic_url = $ship['pic_url'];
             $pic_urls = DB::select('select * from ship_url WHERE ship_id = ?',[$pic_url]);
             $ship['pic_url'] = $pic_urls;
-            $test[] = $ship;
+            $ships[$ship] = $ship;
         }
 
-        return $test;
+        return $ships;
     }
 }
