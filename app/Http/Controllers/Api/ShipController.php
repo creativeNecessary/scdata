eb7b4b3acfb335005e3704b3b65ea55bc27d92ca
EricAlexEX <>
2018-03-21 17:02:28 +0800

    add get ship info api

--- app/Http/Controllers/Api/ShipController.php
+++ app/Http/Controllers/Api/ShipController.php
@@ -25,10 +25,11 @@ class ShipController extends Controller
     public function getShipDetail(Request $request){
         $input=$request->only(['ship_id']);
         $ship_id = $input['ship_id'];
-        $ship = DB::select('select * from ship_en WHERE id = ?',[$ship_id]);
+        $ships = DB::select('select * from ship_en WHERE id = ?',[$ship_id]);
 //        $ship = json_decode(json_encode($ship));
 //        $ship_urls = DB::select('select url from ship_url WHERE ship_id = ? AND type = ?',[$ship_id,'image']);
 //        $ship_urls = json_decode(json_encode($ship_urls));
+        $ship = $ships[0];
         return $ship;
     }
 }
