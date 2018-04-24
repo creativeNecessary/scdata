6ca8533931f98c146b77cccdcea912ca4fc18c3e
EricAlexEX <>
2018-04-24 13:13:19 +0800

    update model api

--- app/Http/Controllers/Api/ShipController.php
+++ app/Http/Controllers/Api/ShipController.php
@@ -19,95 +19,100 @@ use stdClass;
 class ShipController extends Controller
 {
     //
-    public function getShipList(Request $request){
+    public function getShipList(Request $request)
+    {
 
-        $input=$request->only(['page_num','per_page']);
+        $input = $request->only(['page_num', 'per_page']);
         $page_num = $input['page_num'];
         $per_page = $input['per_page'];
         $start = $page_num * $per_page;
-        $ships = DB::select('select id,name,url,icon,size,focus,max_crew,length from ship_en LIMIT ? , ?',[$start,$per_page]);
+        $ships = DB::select('select id,name,url,icon,size,focus,max_crew,length from ship_en LIMIT ? , ?', [$start, $per_page]);
         $ships = json_decode(json_encode($ships));
 
         return $this->onSuccess($ships);
     }
 
     //
-    public function getShipDetail(Request $request){
-        $input=$request->only(['ship_id']);
+    public function getShipDetail(Request $request)
+    {
+        $input = $request->only(['ship_id']);
         $ship_id = $input['ship_id'];
         $ship = ShipModel::find($ship_id);
-        $ship_url = ShipUrl::select('url')->where([['ship_id',$ship_id],['type','image']])->get();
-        $manufacturer = ManufacturerModel::where('id',$ship->manufacturer)->get();
+        $ship_url = ShipUrl::select('url')->where([['ship_id', $ship_id], ['type', 'image']])->get();
+        $manufacturer = ManufacturerModel::where('id', $ship->manufacturer)->get();
 
 
         $ship->setImageUrl($ship_url);
         $ship->setManufacturer($manufacturer);
 
-        $this->initShipEquipment($ship,$ship_id,'avionics','avionic');
-        $this->initShipEquipment($ship,$ship_id,'modular','modular');
-        $this->initShipEquipment($ship,$ship_id,'propulsion','propulsion');
-        $this->initShipEquipment($ship,$ship_id,'thrusters','thruster');
-        $this->initShipEquipment($ship,$ship_id,'weapons','weapon');
+        $this->initShipEquipment($ship, $ship_id, 'avionics', 'avionic');
+        $this->initShipEquipment($ship, $ship_id, 'modular', 'modular');
+        $this->initShipEquipment($ship, $ship_id, 'propulsion', 'propulsion');
+        $this->initShipEquipment($ship, $ship_id, 'thrusters', 'thruster');
+        $this->initShipEquipment($ship, $ship_id, 'weapons', 'weapon');
         return $this->onSuccess($ship);
 
     }
 
-    private function initShipEquipment($ship,$ship_id,$ship_field,$tag){
-        $ship_equipments =  ShipEquipment::where([['ship_id',$ship_id],['tag',$tag]])->get();
-        foreach ($ship_equipments as $ship_equipment => $value){
-            $equipment_id =  $value->equipment;
+    private function initShipEquipment($ship, $ship_id, $ship_field, $tag)
+    {
+        $ship_equipments = ShipEquipment::where([['ship_id', $ship_id], ['tag', $tag]])->get();
+        foreach ($ship_equipments as $ship_equipment => $value) {
+            $equipment_id = $value->equipment;
             $equipment = Equipment::find($equipment_id);
-            if($equipment != null){
+            if ($equipment != null) {
                 $ship_equipments[$ship_equipment]->setEquipment($equipment);
-            }else{
+            } else {
                 $obj = new stdClass();
                 $ship_equipments[$ship_equipment]->setEquipment($obj);
             }
         }
-        $ship->setShipEquipment($ship_field,$ship_equipments);
+        $ship->setShipEquipment($ship_field, $ship_equipments);
     }
 
 
-    public function getSTLFile(Request $request){
-        $input=$request->only(['ship_id']);
+    public function getSTLFile(Request $request)
+    {
+        $input = $request->only(['ship_id']);
         $ship_id = $input['ship_id'];
         //获取飞船id从数据库查询
 
 
-        $model_path = ShipModel::where('id',$ship_id)->value('model3d_url');
-        $end_str = strrchr($model_path,'/');
+        $model_path = ShipModel::where('id', $ship_id)->value('model3d_url');
+        $end_str = strrchr($model_path, '/');
 
-        if(strlen($end_str) <= 5){
+        if (strlen($end_str) <= 5) {
             return $this->onFailure(ResponseApi::$SERVICE_ERROR);
         }
-        $sub_file_name = substr($end_str,1,strlen($end_str)-5);
-        $filename = $sub_file_name.'.stl';
+        $sub_file_name = substr($end_str, 1, strlen($end_str) - 5);
+        $filename = $sub_file_name . '.stl';
 
 
-        $path = resource_path('media/ctmfiles/'.$filename);
-        if($path == null){
+        $path = resource_path('media/ctmfiles/' . $filename);
+        if ($path == null) {
             return $this->onFailure(ResponseApi::$SERVICE_ERROR);
         }
-        if(strlen($path) <= 0){
+        if (strlen($path) <= 0) {
             return $this->onFailure(ResponseApi::$SERVICE_ERROR);
         }
         $size = filesize($path);
 
         $headers = [
             'Content-Type' => 'application/vnd.ms-pki.stl',
+            'Content-Length' => $size,
         ];
-        return $this->onSuccess($size);
-//        return response()->download($path,$filename,$headers);
+        return response()->download($path,$filename,$headers);
     }
 
-    public function getImageFile($filename){
+    public function getImageFile($filename)
+    {
 
-        $path = resource_path('media/image/'.$filename);
+        $path = resource_path('media/image/' . $filename);
         $headers = [
             'Content-Type' => 'image/png',
         ];
 
-        return response()->download($path,$filename,$headers);
+        return response()->download($path, $filename, $headers);
     }
 
 }
