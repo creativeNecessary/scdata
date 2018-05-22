5dcffea4d79be554b83619587d2f4b71821fca5a
eric <eirc@favalue.cn>
2018-05-22 14:59:41 +0800

    update model api

--- app/Http/Controllers/Api/CheckUpdateController.php
+++ app/Http/Controllers/Api/CheckUpdateController.php
@@ -40,7 +40,7 @@ class CheckUpdateController extends Controller
             $obj->code = CheckUpdateController::$NOW_LAST_VERSION;
         }
 
-        return $this->onSuccess($obj);
+        return $this->onSuccess($this->nowVersionCode);
 
     }
 
