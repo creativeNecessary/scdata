2edef4486c9791da1ed07c3549dde5e775956e68
eric <eirc@favalue.cn>
2018-05-23 13:12:03 +0800

    update model api

--- app/Http/Controllers/Api/CheckUpdateController.php
+++ app/Http/Controllers/Api/CheckUpdateController.php
@@ -61,7 +61,7 @@ class CheckUpdateController extends Controller
 //        ];
 
 //        return response()->download($path, $filename, $headers);
-        return $this->onSuccess(gettype($app_last_version));
+        return $this->onSuccess($app_last_version);
     }
 
 }
