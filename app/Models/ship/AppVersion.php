40b9242946d34bc10a92e4eaac91c176762436aa
eric <eirc@favalue.cn>
2018-05-23 13:35:18 +0800

    update model api

--- app/Models/ship/AppVersion.php
+++ app/Models/ship/AppVersion.php
@@ -17,6 +17,19 @@ class AppVersion extends Model
     protected $table = "app_version";
     public $timestamps = false;
 
+    public function get_version_code(){
+        return $this->attributes['version_code'];
+    }
 
+    public function get_version_name(){
+        return $this->attributes['version_name'];
+    }
 
+    public function get_version_focus(){
+        return $this->attributes['version_focus'];
+    }
+
+    public function get_apk_file_name(){
+        return $this->attributes['apk_file_name'];
+    }
 }
