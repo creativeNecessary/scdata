d0818440e968f86578836aa7160d280ff57a5f86
eric <eirc@favalue.cn>
2018-05-23 13:40:08 +0800

    update model api

--- app/Http/Controllers/Api/CheckUpdateController.php
+++ app/Http/Controllers/Api/CheckUpdateController.php
@@ -53,7 +53,7 @@ class CheckUpdateController extends Controller
     public function getUpdateApkFile()
     {
         $app_last_version = AppVersion::orderBy('id','desc')->take(1)->get();
-        $filename = $app_last_version->get_apk_file_name();
+//        $filename = $app_last_version->get_apk_file_name();
 
 //        $path = resource_path($app_last_version['apk_file_name']);
 //        $headers = [
